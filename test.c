ed4896cd7ca4ae45a80c848f18479e95eee39341
pommeJedusor <inhumain42@protonmail.com>
2023-08-07 00:06:09 +0200

    add the first function to make the tests

--- test.c
+++ test.c
@@ -20,27 +20,79 @@ int test(char* file_name)
     char* file_contents = malloc(sb.st_size);
     fread(file_contents, sb.st_size, 1, input_file);
 
+    //printf("%s",file_contents);
+
     //make the tests
+    int total_score=0;
+    int total_test=0;
     int game_len;
     int file_pos = 0;
+    int ex_file_pos;
+    char is_first_part;
+    int score = 0;
+    char more_less;
+    int result;
     while (1)
     {
-        //calcule la longeur de la game
+        //calcule la longeur de la game et récupère le score
+        ex_file_pos = file_pos;
+        is_first_part = 1;
         game_len = 0;
+        more_less = 1;
+        score = 0;
         while (1)
         {
-            if (file_contents[game_len+file_pos]=='\n')
+            if (file_contents[file_pos]=='\n')
             {
-                file_pos+= game_len;
+                file_pos++;
+                break;
             }
-            else if (file_contents[game_len+file_pos]==' ')
+            else if (file_contents[file_pos]==' ')
+            {
+                is_first_part = 0;
+                if (file_contents[file_pos+1]=='-')
+                {
+                    more_less = -1;
+                }
+            }
+            else if (file_contents[file_pos]!='-' && is_first_part==0)
+            {
+                score = score * 10 + (file_contents[file_pos]-48);
+            }
+            else if (is_first_part)
+            {
+                game_len++;
+            }
+            if (file_contents[file_pos]=='\0')
+            {
+                printf("yop");
+                break;
+            }
+            file_pos++;
+
         }
+        Grid* grid = grid_init(file_contents+ex_file_pos, game_len);
+        result = connect4(grid);
+        score*=more_less;
+
+        total_test++;
+        if (result==score)
+        {
+            total_score++;
+        }
+
+
+        if (file_contents[file_pos]=='\0')
+        {
+            break;
+        }
+
     }
 
     fclose(input_file);
     free(file_contents);
 
-
+    printf("test level 1: %d/%d de réussis\n",total_score,total_test);
     return 0;
 }
 
